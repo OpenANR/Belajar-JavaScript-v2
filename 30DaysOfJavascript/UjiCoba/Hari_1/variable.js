9e668211ea9742ef219894fdbef7729890af2be3
OpenANR <nuzulnazil14@gmail.com>
2026-07-10 23:44:57 +0700

    Perbaikai

---
+++ 30DaysOfJavascript/UjiCoba/Hari_1/variable.js
@@ -0,0 +1,33 @@
+let variabel1 = 'Nuzul';
+let variabel2 = 2026;
+let variabel3 = true;
+let variable4 = false;
+let variabel5 = null;
+let variabel6 = undefined;
+
+console.log(typeof variabel1, variabel2, variabel3, variable4, variabel5, variabel6);
+
+let item1, item2, item3, item4
+console.log(item1, item2, item3, item4);
+
+let data1 = 'Data 1', data2 = 'Data 2', data3 = 'Data 3', data4 = 'Data 4';
+console.log(data1, data2, data3, data4);
+
+let namaDepan = 'Ahmad';
+let namaTengah = 'Nuzulur';
+let namaAkhir = 'Rozaq';
+let negara = 'Indonesia';
+let usia = 20;
+let statusMenikah = 'Belum menikah';
+
+console.log(`
+        Perkenalkan nama saya ${namaDepan} ${namaTengah} ${namaAkhir}.
+        Saya berasal dari Negara ${negara}.
+        Usia saya adalah ${usia}.
+        Status pernikahan ${statusMenikah}.
+    `)
+
+let umurSaya = 20;
+let umurKamu = 18;
+console.log(`Saya berumur ${umurSaya} tahun.`);
+console.log(`Kamu berumur ${umurKamu} tahun.`);
